5434de862424182f0549a5bedbd78df27d856ca1
sadah <sadaaki.hirai@gmail.com>
2023-09-16 08:26:46 +0000

    Add 011

--- src/011/main.go
+++ src/011/main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
 
-func solve(N int64) {
+func isPrime(n int64) bool {
+	for i := int64(2); i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
 
+func solve(N int64) {
+	for i := int64(2); i <= N; i++ {
+		if isPrime(i) {
+			fmt.Print(i, " ")
+		}
+	}
+	fmt.Println()
 }
 
 func main() {
@@ -17,7 +32,7 @@ func main() {
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	var N int64
-    scanner.Scan()
-    N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	scanner.Scan()
+	N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
 	solve(N)
 }
