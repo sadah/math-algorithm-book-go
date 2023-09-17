56f475ad7ebd3351cc870b798c677e3365b337f8
sadah <sadaaki.hirai@gmail.com>
2023-09-17 02:05:00 +0000

    Add 013

--- src/013/main.go
+++ src/013/main.go
@@ -2,12 +2,33 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
 func solve(N int64) {
+	divs := divisors(N)
+	for _, d := range divs {
+		fmt.Println(d)
+	}
+}
 
+func divisors(N int64) []int64 {
+	var ret []int64
+	for i := int64(1); i*i <= N; i++ {
+		if N%i == 0 {
+			ret = append(ret, i)
+			if i*i != N {
+				ret = append(ret, N/i)
+			}
+		}
+	}
+	sort.Slice(ret, func(i, j int) bool {
+		return ret[i] < ret[j]
+	})
+	return ret
 }
 
 func main() {
@@ -17,7 +38,7 @@ func main() {
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	var N int64
-    scanner.Scan()
-    N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	scanner.Scan()
+	N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
 	solve(N)
 }
