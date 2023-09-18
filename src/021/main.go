586e6003380592a1adef81c55dc3712ace2a6755
sadah <sadaaki.hirai@gmail.com>
2023-09-18 04:44:57 +0000

    Add 021

--- src/021/main.go
+++ src/021/main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
 
 func solve(n int64, r int64) {
+	fmt.Println(combination(n, r))
+}
 
+// combination returns nCr.
+func combination(n, r int64) int64 {
+	if n < r {
+		return 0
+	}
+	if n == r {
+		return 1
+	}
+	if r == 1 {
+		return n
+	}
+	return combination(n-1, r) + combination(n-1, r-1)
 }
 
 func main() {
@@ -17,10 +32,10 @@ func main() {
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	var n int64
-    scanner.Scan()
-    n, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    var r int64
-    scanner.Scan()
-    r, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	scanner.Scan()
+	n, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var r int64
+	scanner.Scan()
+	r, _ = strconv.ParseInt(scanner.Text(), 10, 64)
 	solve(n, r)
 }
