451665211a77ffc04c8e283ece2668a5c3f310e8
sadah <sadaaki.hirai@gmail.com>
2023-09-20 06:54:26 +0000

    Add 036

--- src/036/main.go
+++ src/036/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"math"
 	"os"
 	"strconv"
 )
 
 func solve(A int64, B int64, H int64, M int64) {
-
+	rad := math.Pi * 2 * (float64(H)/12 + float64(M)/60/12 - float64(M)/60)
+	ans := math.Sqrt(float64(A*A+B*B) - 2*float64(A*B)*math.Cos(rad))
+	fmt.Println(ans)
 }
 
 func main() {
@@ -17,16 +21,16 @@ func main() {
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	var A int64
-    scanner.Scan()
-    A, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    var B int64
-    scanner.Scan()
-    B, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    var H int64
-    scanner.Scan()
-    H, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    var M int64
-    scanner.Scan()
-    M, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	scanner.Scan()
+	A, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var B int64
+	scanner.Scan()
+	B, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var H int64
+	scanner.Scan()
+	H, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var M int64
+	scanner.Scan()
+	M, _ = strconv.ParseInt(scanner.Text(), 10, 64)
 	solve(A, B, H, M)
 }
