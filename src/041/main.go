71fb01039444876299ee87663d432776f3962af9
sadah <sadaaki.hirai@gmail.com>
2023-09-21 06:10:19 +0000

    Add 041

--- src/041/main.go
+++ src/041/main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
 
 func solve(T int64, N int64, L []int64, R []int64) {
-
+	nums := make([]int64, T+2)
+	for i := int64(0); i < N; i++ {
+		nums[L[i]+1]++
+		nums[R[i]+1]--
+	}
+	for i := int64(0); i < T; i++ {
+		nums[i+1] += nums[i]
+		fmt.Println(nums[i+1])
+	}
 }
 
 func main() {
@@ -17,18 +26,18 @@ func main() {
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	var T int64
-    scanner.Scan()
-    T, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    var N int64
-    scanner.Scan()
-    N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    L := make([]int64, N)
-    R := make([]int64, N)
-    for i := int64(0); i < N; i++ {
-        scanner.Scan()
-        L[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
-        scanner.Scan()
-        R[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    }
+	scanner.Scan()
+	T, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var N int64
+	scanner.Scan()
+	N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	L := make([]int64, N)
+	R := make([]int64, N)
+	for i := int64(0); i < N; i++ {
+		scanner.Scan()
+		L[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
+		scanner.Scan()
+		R[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	}
 	solve(T, N, L, R)
 }
