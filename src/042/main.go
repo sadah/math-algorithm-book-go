eb5677686c2aad754485caa9fd82a3bf72f2d99c
sadah <sadaaki.hirai@gmail.com>
2023-09-22 01:08:13 +0000

    Add 042

--- src/042/main.go
+++ src/042/main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
 
+// https://kyo-pro.hatenablog.com/entry/ABC172D
 func solve(N int64) {
-
+	sum := int64(0)
+	for i := int64(1); i <= N; i++ {
+		sum += (((N/i)*(N/i) + (N / i)) / 2) * i
+	}
+	fmt.Println(sum)
 }
 
 func main() {
@@ -17,7 +23,7 @@ func main() {
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	var N int64
-    scanner.Scan()
-    N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	scanner.Scan()
+	N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
 	solve(N)
 }
