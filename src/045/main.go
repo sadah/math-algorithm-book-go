df7f5ef1dd2bdf91eb5f0b61e23b257e8bf576a0
sadah <sadaaki.hirai@gmail.com>
2023-09-25 05:28:11 +0000

    Add 045

--- src/045/main.go
+++ src/045/main.go
@@ -2,12 +2,35 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
 
 func solve(N int64, M int64, a []int64, b []int64) {
+	adj := adjacencyList(N, M, a, b)
+	sum := int64(0)
+	for i, v := range adj {
+		count := 0
+		for _, u := range v {
+			if int64(i) > u {
+				count++
+			}
+		}
+		if count == 1 {
+			sum++
+		}
+	}
+	fmt.Println(sum)
+}
 
+func adjacencyList(N int64, M int64, A []int64, B []int64) (adj [][]int64) {
+	adj = make([][]int64, N)
+	for i := int64(0); i < M; i++ {
+		adj[A[i]-1] = append(adj[A[i]-1], B[i]-1)
+		adj[B[i]-1] = append(adj[B[i]-1], A[i]-1)
+	}
+	return
 }
 
 func main() {
@@ -17,18 +40,18 @@ func main() {
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	var N int64
-    scanner.Scan()
-    N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    var M int64
-    scanner.Scan()
-    M, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    a := make([]int64, M)
-    b := make([]int64, M)
-    for i := int64(0); i < M; i++ {
-        scanner.Scan()
-        a[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
-        scanner.Scan()
-        b[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    }
+	scanner.Scan()
+	N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var M int64
+	scanner.Scan()
+	M, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	a := make([]int64, M)
+	b := make([]int64, M)
+	for i := int64(0); i < M; i++ {
+		scanner.Scan()
+		a[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
+		scanner.Scan()
+		b[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	}
 	solve(N, M, a, b)
 }
