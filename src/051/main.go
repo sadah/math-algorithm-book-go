9fcc9111ffeb4611c4eb1332b99518c2e0e905a9
sadah <sadaaki.hirai@gmail.com>
2023-09-29 06:56:02 +0000

    Update 051

--- src/051/main.go
+++ src/051/main.go
@@ -28,30 +28,34 @@ func solve(X int64, Y int64) {
 		denominator = (denominator * int64(i)) % MOD
 	}
 
-	ans := division(numerator, denominator, MOD)
+	ans := modDiv(numerator, denominator, MOD)
 	fmt.Println(ans)
 }
 
-func modpow(a, b, m int64) int64 {
-	// 繰り返し二乗法（p は a^1, a^2, a^4, a^8, ... といった値をとる）
-	p := a
-	answer := int64(1)
-	for i := 0; i < 30; i++ {
-		if (b & (1 << i)) != 0 {
-			answer = (answer * p) % m
-		}
-		p = (p * p) % m
+// a の n 乗を m で割った余りを返す関数
+func modPower(a, n, m int64) int64 {
+	// n が 0 の場合、1 を返す
+	if n == 0 {
+		return 1
 	}
-	return answer
+
+	// n が偶数の場合、a^n = (a^(n/2))^2 を利用して計算
+	if n%2 == 0 {
+		halfPower := modPower(a, n/2, m)
+		return (halfPower * halfPower) % m
+	}
+
+	// n が奇数の場合、x^n = x * (x^(n-1)) を利用して計算
+	return (a * modPower(a, n-1, m)) % m
 }
 
-func division(a, b, m int64) int64 {
-	// division(a, b, m) は a÷b mod m を返す関数
-	// a÷b = a×b^-1 となる
-	// b^-1 は b の逆元である
-	// b^-1 は b^(m-2) mod m で求められる
-	// これはフェルマーの小定理より成り立つ
-	return (a * modpow(b, m-2, m)) % m
+// modDiv(a, b, m) は a÷b mod m を返す関数
+// a÷b = a×b^-1 となる
+// b^-1 は b の逆元である
+// b^-1 は b^(m-2) mod m で求められる
+// これはフェルマーの小定理より成り立つ
+func modDiv(a, b, m int64) int64 {
+	return (a * modPower(b, m-2, m)) % m
 }
 
 func main() {
