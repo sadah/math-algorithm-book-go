585f4da042edbe1bfa7e4fe5dfccbac84f286180
sadah <sadaaki.hirai@gmail.com>
2023-09-30 22:53:34 +0000

    Add 059

--- src/059/main.go
+++ src/059/main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
 
 func solve(N int64) {
-
+	if N%4 == 1 {
+		fmt.Println(2)
+	} else if N%4 == 2 {
+		fmt.Println(4)
+	} else if N%4 == 3 {
+		fmt.Println(8)
+	} else if N%4 == 0 {
+		fmt.Println(6)
+	}
 }
 
 func main() {
@@ -17,7 +26,7 @@ func main() {
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	var N int64
-    scanner.Scan()
-    N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	scanner.Scan()
+	N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
 	solve(N)
 }
