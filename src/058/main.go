afbed2284d371bb5ebd48aa8e74ed692cdda58f6
sadah <sadaaki.hirai@gmail.com>
2023-09-30 05:39:54 +0000

    Add 058

--- src/058/main.go
+++ src/058/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"math"
 	"os"
 	"strconv"
 )
@@ -10,7 +12,16 @@ const YES = "Yes"
 const NO = "No"
 
 func solve(N int64, X int64, Y int64) {
-
+	sum := int64(math.Abs(float64(X)) + math.Abs(float64(Y)))
+	if sum > N {
+		fmt.Println(NO)
+		return
+	}
+	if (X+Y)%2 != N%2 {
+		fmt.Println(NO)
+		return
+	}
+	fmt.Println(YES)
 }
 
 func main() {
@@ -20,13 +31,13 @@ func main() {
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	var N int64
-    scanner.Scan()
-    N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    var X int64
-    scanner.Scan()
-    X, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    var Y int64
-    scanner.Scan()
-    Y, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	scanner.Scan()
+	N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var X int64
+	scanner.Scan()
+	X, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var Y int64
+	scanner.Scan()
+	Y, _ = strconv.ParseInt(scanner.Text(), 10, 64)
 	solve(N, X, Y)
 }
