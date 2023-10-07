2312ccc343939f0fd5c7ee50cba2d2f0f90ad00e
sadah <sadaaki.hirai@gmail.com>
2023-10-07 09:46:11 +0000

    Add 065

--- src/065/main.go
+++ src/065/main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
 
 func solve(H int64, W int64) {
-
+	if H == 1 || W == 1 {
+		fmt.Println(1)
+	} else {
+		fmt.Println((H*W + 1) / 2)
+	}
 }
 
 func main() {
@@ -17,10 +22,10 @@ func main() {
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	var H int64
-    scanner.Scan()
-    H, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    var W int64
-    scanner.Scan()
-    W, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	scanner.Scan()
+	H, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var W int64
+	scanner.Scan()
+	W, _ = strconv.ParseInt(scanner.Text(), 10, 64)
 	solve(H, W)
 }
