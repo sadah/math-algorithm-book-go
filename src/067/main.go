31e9ff29f19e3c83641aa3032307d67bf02528b7
sadah <sadaaki.hirai@gmail.com>
2023-10-07 09:39:58 +0000

    Add 067

--- src/067/main.go
+++ src/067/main.go
@@ -2,35 +2,61 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func solve(H int64, W int64, A [][]int64) {
+	rowSum := make([]int64, H)
+	colSum := make([]int64, W)
 
+	// Calculate row sum
+	for i := int64(0); i < H; i++ {
+		sum := int64(0)
+		for j := int64(0); j < W; j++ {
+			sum += A[i][j]
+		}
+		rowSum[i] = sum
+	}
+
+	// Calculate column sum
+	for j := int64(0); j < W; j++ {
+		sum := int64(0)
+		for i := int64(0); i < H; i++ {
+			sum += A[i][j]
+		}
+		colSum[j] = sum
+	}
+
+	for i := int64(0); i < H; i++ {
+		ans := make([]string, W)
+		for j := int64(0); j < W; j++ {
+			ans[j] = strconv.Itoa(int(rowSum[i] + colSum[j] - A[i][j]))
+		}
+		fmt.Println(strings.Join(ans, " "))
+	}
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	const initialBufSize = 4096
-	const maxBufSize = 1000000
-	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	var H int64
-    scanner.Scan()
-    H, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    var W int64
-    scanner.Scan()
-    W, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    A := make([][]int64, H)
-    for i := int64(0); i < H; i++ {
-    	A[i] = make([]int64, W)
-    }
-    for i := int64(0); i < H; i++ {
-        for j := int64(0); j < W; j++ {
-            scanner.Scan()
-            A[i][j], _ = strconv.ParseInt(scanner.Text(), 10, 64)
-        }
-    }
+	scanner.Scan()
+	H, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var W int64
+	scanner.Scan()
+	W, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	A := make([][]int64, H)
+	for i := int64(0); i < H; i++ {
+		A[i] = make([]int64, W)
+	}
+	for i := int64(0); i < H; i++ {
+		for j := int64(0); j < W; j++ {
+			scanner.Scan()
+			A[i][j], _ = strconv.ParseInt(scanner.Text(), 10, 64)
+		}
+	}
 	solve(H, W, A)
 }
