2cb0e2cf2fc9892349e5122659a5b8815e035cc1
sadah <sadaaki.hirai@gmail.com>
2023-10-15 02:50:11 +0000

    Add 073

--- src/073/main.go
+++ src/073/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -9,7 +10,17 @@ import (
 const MOD = 1000000007
 
 func solve(N int64, A []int64) {
+	modPower := make([]int64, N)
+	modPower[0] = 1
+	for i := int64(1); i < N; i++ {
+		modPower[i] = 2 * modPower[i-1] % MOD
+	}
 
+	ans := int64(0)
+	for i := int64(0); i < N; i++ {
+		ans = (ans + modPower[i]*A[i]) % MOD
+	}
+	fmt.Println(ans)
 }
 
 func main() {
@@ -19,12 +30,12 @@ func main() {
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	var N int64
-    scanner.Scan()
-    N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    A := make([]int64, N)
-    for i := int64(0); i < N; i++ {
-        scanner.Scan()
-        A[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    }
+	scanner.Scan()
+	N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	A := make([]int64, N)
+	for i := int64(0); i < N; i++ {
+		scanner.Scan()
+		A[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	}
 	solve(N, A)
 }
