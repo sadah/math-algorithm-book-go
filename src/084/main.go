6323291c7887898c8750650c553d43fad3e3ed96
sadah <sadaaki.hirai@gmail.com>
2023-11-05 08:15:26 +0000

    Add 084

--- src/084/main.go
+++ src/084/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -10,7 +11,13 @@ const YES = "Yes"
 const NO = "No"
 
 func solve(a int64, b int64, c int64) {
-
+	if c-a-b < 0 {
+		fmt.Println(NO)
+	} else if 4*a*b < (c-a-b)*(c-a-b) {
+		fmt.Println(YES)
+	} else {
+		fmt.Println(NO)
+	}
 }
 
 func main() {
@@ -20,13 +27,13 @@ func main() {
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	var a int64
-    scanner.Scan()
-    a, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    var b int64
-    scanner.Scan()
-    b, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    var c int64
-    scanner.Scan()
-    c, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	scanner.Scan()
+	a, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var b int64
+	scanner.Scan()
+	b, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var c int64
+	scanner.Scan()
+	c, _ = strconv.ParseInt(scanner.Text(), 10, 64)
 	solve(a, b, c)
 }
