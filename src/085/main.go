292c8c8e3b26b8363614a0309b5942f61ce4ddc5
sadah <sadaaki.hirai@gmail.com>
2023-11-12 01:55:18 +0000

    Add 085

--- src/085/main.go
+++ src/085/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -10,7 +11,20 @@ const YES = "Yes"
 const NO = "No"
 
 func solve(N int64, X int64, Y int64) {
-
+	var a, b, c, d int64
+	for a = int64(1); a <= N; a++ {
+		for b = a; b <= N; b++ {
+			for c = b; c <= N; c++ {
+				for d = c; d <= N; d++ {
+					if a+b+c+d == X && a*b*c*d == Y {
+						fmt.Println(YES)
+						return
+					}
+				}
+			}
+		}
+	}
+	fmt.Println(NO)
 }
 
 func main() {
@@ -20,13 +34,13 @@ func main() {
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	var N int64
-    scanner.Scan()
-    N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    var X int64
-    scanner.Scan()
-    X, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    var Y int64
-    scanner.Scan()
-    Y, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	scanner.Scan()
+	N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var X int64
+	scanner.Scan()
+	X, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var Y int64
+	scanner.Scan()
+	Y, _ = strconv.ParseInt(scanner.Text(), 10, 64)
 	solve(N, X, Y)
 }
