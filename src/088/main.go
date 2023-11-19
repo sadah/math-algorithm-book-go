ef53ab4e67e0c105c4abc1deb1e871c1dda80c43
sadah <sadaaki.hirai@gmail.com>
2023-11-19 04:49:18 +0000

    Add 088

--- src/088/main.go
+++ src/088/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -9,7 +10,10 @@ import (
 const MOD = 998244353
 
 func solve(A int64, B int64, C int64) {
-
+	a := A * (A + 1) / 2 % MOD
+	b := B * (B + 1) / 2 % MOD
+	c := C * (C + 1) / 2 % MOD
+	fmt.Println(a * b % MOD * c % MOD)
 }
 
 func main() {
@@ -19,13 +23,13 @@ func main() {
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	var A int64
-    scanner.Scan()
-    A, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    var B int64
-    scanner.Scan()
-    B, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    var C int64
-    scanner.Scan()
-    C, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	scanner.Scan()
+	A, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var B int64
+	scanner.Scan()
+	B, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var C int64
+	scanner.Scan()
+	C, _ = strconv.ParseInt(scanner.Text(), 10, 64)
 	solve(A, B, C)
 }
