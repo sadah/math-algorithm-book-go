6e0e3fe9e66613ca07aad28b7bd25a53ba4d008a
sadah <sadaaki.hirai@gmail.com>
2023-11-19 05:23:04 +0000

    Add 090

--- src/090/main.go
+++ src/090/main.go
@@ -2,12 +2,49 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
 
-func solve(N int64, B int64) {
+var fmCand map[int64]struct{}
 
+func solve(N int64, B int64) {
+	fmCand = make(map[int64]struct{})
+	ans := int64(0)
+	f(0, 0)
+	for fm := range fmCand {
+		m := fm + B
+		prodM := product(m)
+		if m-prodM == B && m <= N {
+			ans += 1
+		}
+	}
+	fmt.Println(ans)
+}
+
+func product(m int64) int64 {
+	if m == 0 {
+		return 0
+	} else {
+		ans := int64(1)
+		for m >= 1 {
+			ans *= m % 10
+			m /= 10
+		}
+		return ans
+	}
+}
+
+func f(digit int, m int64) {
+	if digit == 11 {
+		fmCand[product(m)] = struct{}{}
+		return
+	}
+	minValue := m % 10
+	for i := minValue; i <= 9; i++ {
+		f(digit+1, 10*m+i)
+	}
 }
 
 func main() {
@@ -17,10 +54,10 @@ func main() {
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	var N int64
-    scanner.Scan()
-    N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    var B int64
-    scanner.Scan()
-    B, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	scanner.Scan()
+	N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var B int64
+	scanner.Scan()
+	B, _ = strconv.ParseInt(scanner.Text(), 10, 64)
 	solve(N, B)
 }
