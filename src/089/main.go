ec95032c1aa79352af3876b45ae5e51d2aa7f1f2
sadah <sadaaki.hirai@gmail.com>
2023-11-19 04:54:29 +0000

    Add 089

--- src/089/main.go
+++ src/089/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -10,7 +11,20 @@ const YES = "Yes"
 const NO = "No"
 
 func solve(a int64, b int64, c int64) {
+	if c == 1 {
+		fmt.Println(NO)
+		return
+	}
 
+	v := int64(1)
+	for i := int64(1); i <= b; i++ {
+		if a/c < v {
+			fmt.Println(YES)
+			return
+		}
+		v *= c
+	}
+	fmt.Println(NO)
 }
 
 func main() {
@@ -20,13 +34,13 @@ func main() {
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	var a int64
-    scanner.Scan()
-    a, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    var b int64
-    scanner.Scan()
-    b, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    var c int64
-    scanner.Scan()
-    c, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	scanner.Scan()
+	a, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var b int64
+	scanner.Scan()
+	b, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var c int64
+	scanner.Scan()
+	c, _ = strconv.ParseInt(scanner.Text(), 10, 64)
 	solve(a, b, c)
 }
