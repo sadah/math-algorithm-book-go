68bea13b2f3956c13be5d634c8fba5276a27a960
sadah <sadaaki.hirai@gmail.com>
2023-11-19 04:42:07 +0000

    Add 086

--- src/086/main.go
+++ src/086/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -10,7 +11,20 @@ const YES = "Yes"
 const NO = "No"
 
 func solve(N int64, S string) {
-
+	depth := 0
+	for i := int64(0); i < N; i++ {
+		if S[i:i+1] == "(" {
+			depth += 1
+		}
+		if S[i:i+1] == ")" {
+			depth -= 1
+		}
+		if depth < 0 {
+			fmt.Println(NO)
+			return
+		}
+	}
+	fmt.Println(YES)
 }
 
 func main() {
@@ -20,10 +34,10 @@ func main() {
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	var N int64
-    scanner.Scan()
-    N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    var S string
-    scanner.Scan()
-    S = scanner.Text()
+	scanner.Scan()
+	N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var S string
+	scanner.Scan()
+	S = scanner.Text()
 	solve(N, S)
 }
